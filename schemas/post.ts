c18b9f329e3e84523c6056c673a208ee1fb49579
silqwer <silqwer@gmail.com>
2023-07-09 11:31:14 +0900

    feat: post schemas

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,1 +1,61 @@
-export default {}
+import {Rule} from 'sanity'
+
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
+    },
+    {
+      title: 'Photo',
+      name: 'photo',
+      type: 'image',
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (rule: Rule) => rule.unique(),
+    },
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [
+                {
+                  type: 'user',
+                },
+              ],
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
